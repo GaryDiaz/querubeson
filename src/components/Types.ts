2ddc0ba735a7c99d0f14c30f658b15d374f830e1
Gary Diaz <garyking1982@gmail.com>
2024-05-27 23:38:51 -0400

    agregando componentes de videos y las modificaciones de los componentes afectados por los cambios de fondiguración

---
+++ src/components/Types.ts
@@ -0,0 +1,9 @@
+export type VideoData = {
+  idVideo: string;
+  title: string;
+  publicationDate: Date;
+  author: string;
+  singer: string;
+  description: string;
+  tags: string[];
+};
---
+++ src/components/VideoList.ts
@@ -0,0 +1,33 @@
+import type { VideoData } from "@components/Types";
+export const videoList: VideoData[] = [
+  {
+    idVideo: "y8Rqb-P1NvI",
+    title: "Mi viejo",
+    publicationDate: new Date("2023-06-19"),
+    author: "Arnaldo Quintero",
+    singer: "Gary Díaz",
+    description:
+      "Cover del Tema Mi Viejo, de Adolescentes Orquesta, del Compositor Arnaldo Quintero",
+    tags: ["cover", "homenaje", "mi-viejo", "adolescentes"],
+  },
+  {
+    idVideo: "hPCejhyo8y0",
+    title: "Burrito Sabanero",
+    publicationDate: new Date("2021-12-27"),
+    author: "Hugo Blanco",
+    singer: "Evismar Sofía Rondon y Eviannys María Rondón",
+    description:
+      "Cover del Burrito Sabanero de Hugo Blanco, con fragmentos de Rap de Coros de Amor, producido para el Ministerio de Música Génesis",
+    tags: ["cover", "navidad"],
+  },
+  {
+    idVideo: "pdbj8nDhU6c",
+    title: "Homenaje a Héctor Lavoe",
+    publicationDate: new Date("2021-09-30"),
+    author: "Ruben Blade",
+    singer: "Gary Díaz",
+    description:
+      "Una muestra de algunas de sus canciones más emblemáticas, dos estrofas y algunos soneos de El Cantante, canción compuesta por Ruben Blades, Todos los temas producidos originalmente con el Sello Discográfico de Fania Records",
+    tags: ["cover", "homenaje", "hector-lavoe"],
+  },
+];
